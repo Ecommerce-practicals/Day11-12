3e238a0abc01f017343b18fea0aa6dca57d8e748
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-06-05 22:15:47 +0530

    initialize the project

---
+++ app/src/main/java/fas/ict/app/AppApplication.java
@@ -0,0 +1,13 @@
+package fas.ict.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
